4c63c67a1037ee73ab350a420395e98275d1db5c
mai-thao <mthaominn@gmail.com>
2025-10-12 23:41:37 -0500

    Attempt to connect to PG database

--- book-server/cmd/memory/main.go
+++ book-server/cmd/memory/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+    "log"
+    "database/sql"
+
+    _ "github.com/lib/pq"
     "github.com/gin-gonic/gin"
     "book-server/internal/handler"
 )
 
 func main() {
+    log.Println("Attempting to connect to Postgres database...")
+    connStr := "postgres://adminmt:adminpw@localhost:5432/bookdb?sslmode=disable"
+    db, err := sql.Open("postgres", connStr)
+    defer db.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err = db.Ping(); err != nil {
+        log.Println("Database ping failed!")
+        log.Fatal(err)
+    }
+    log.Println("Successfully connected to Postgres database!")
+
     router := gin.Default()
     handler.RegisterRoutes(router)
     router.Run("localhost:8080")
