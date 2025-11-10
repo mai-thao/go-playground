3c80ab88ee055b03efe67468986ef47483e107c9
mai-thao <mthaominn@gmail.com>
2025-11-09 23:56:41 -0600

    Init unit test for Book Handler

---
+++ book-server/internal/handler/book_handler_test.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+// Test for checking responsive webserver
+func TestPingRoute(t *testing.T) {
+
+}
+
+// Test for GET /books
+func TestGetBooks(t *testing.T) {
+
+}
+
+// Test for GET /books/:id
+func TestGetBookByID(t *testing.T) {
+
+}
+
+// Test for POST /books
+func TestCreateBook(t *testing.T) {
+
+}
+
+// Test for DELETE /books/:id
+func TestDeleteBook(t *testing.T) {
+
+}
